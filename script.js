da8e98c381bb70a370d177cf59267f4b91f206fd
Sep <Hollysoul00@gmail.com>
2023-01-18 13:55:30 +0700

    Fixed Bug

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  for (var person in names) { /* fill in parts of the 'for' loop to loop over names array */
-    //console.log(names[person])  
+  for (var person in names) { 
   
     if ((names[person][0] == "J") || (names[person][0] == "j")) {
-      //console.log("Good Bye " + names[person]);
+      
       byeSpeaker.speak(names[person])
     } 
     else {
-      //console.log("Hello " + names[person]);
+      
       helloSpeaker.speak(names[person])
     }
   }
